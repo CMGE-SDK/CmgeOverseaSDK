aedf595e79a0d79e2910409d66a76a6bce37d244
Huajie Woo <wu.huajie@cmge.com>
2023-12-26 16:40:13 +0800

    Initial commit

---
+++ Example/CmgeOverseaSDK/CMGEViewController.h
@@ -0,0 +1,13 @@
+//
+//  CMGEViewController.h
+//  CmgeOverseaSDK
+//
+//  Created by Huajie Woo on 12/26/2023.
+//  Copyright (c) 2023 Huajie Woo. All rights reserved.
+//
+
+@import UIKit;
+
+@interface CMGEViewController : UIViewController
+
+@end
